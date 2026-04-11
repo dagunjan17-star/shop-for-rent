687d56f1c8f64cd4279355217bd30fd50956a711
Mukesh Kumar <mk8701952@gmail.com>
2026-04-11 13:39:39 +0530

    rewa

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,7 @@ import Footer from "@/components/Footer";
 import ScrollToTop from "@/components/ScrollToTop";
 import { Toaster } from "react-hot-toast";
 import { BlogProvider } from "@/contextapi/BlogContext";
+import Script from "next/script";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -17,23 +18,98 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "Shop for Rent Gurgaon",
-  description: "Find verified Shop for rent in Gurgaon with best deals.",
-};
+ title: "Shops for Rent in Gurgaon | Commercial Retail Space for Rent",
 
+description:
+"Find verified shops for rent in Gurgaon including retail shops, showrooms, and commercial spaces. Explore affordable and premium shop spaces in prime locations with high footfall, modern infrastructure, and flexible lease options.",
+
+keywords: [
+  "shop for rent in gurgaon",
+  "shops for rent gurgaon",
+  "retail shop gurgaon rent",
+  "commercial shop gurgaon",
+  "showroom for rent gurgaon",
+  "shop space gurgaon rent",
+  "commercial space gurgaon rent",
+  "cheap shop for rent gurgaon",
+  "ready to move shop gurgaon",
+  "retail space gurgaon"
+],
+
+  alternates: {
+    canonical: "https://www.shopforrentingurgaon.com/",
+  },
+  verification: {
+    google: "vyf3FEnSVLgMNCbul3J0eumyobmb77Rj8D48aoFTGBg",
+  },
+   icons: {
+      icon: "/favicon.ico",
+      shortcut: "/favicon.ico",
+      apple: "/favicon.ico",
+    },
+};
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable}`}
+    >
+      <head>
+        {/* ✅ Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-W2WZ4NNM');
+            `,
+          }}
+        />
+
+        {/* ✅ Google Analytics (GA4) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-2NCFHM4P1J"
+          strategy="afterInteractive"
+        />
+        <Script id="ga-script" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-2NCFHM4P1J');
+          `}
+        </Script>
+      </head>
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* ✅ GTM NoScript */}
+        
+ <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W2WZ4NNM"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
+
+        {/* ✅ Providers */}
         <PropertyProvider>
           <BlogProvider>
-          <Navbar />
-          {children}
-          <ScrollToTop />
-          <Footer />
-          <Toaster position="top-right" reverseOrder={false} />
+            <Navbar />
+            {children}
+            <ScrollToTop />
+            <Footer />
+            <Toaster position="top-right" reverseOrder={false} />
           </BlogProvider>
         </PropertyProvider>
       </body>
